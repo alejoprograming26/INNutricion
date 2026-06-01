4eff611d8fb0bbeda7a6c7c95321893afb667603
alejoprograming26 <151877512+alejoprograming26@users.noreply.github.com>
2026-06-01 08:08:10 -0400

    Ultimas Mejoras

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use App\Models\Ajuste;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,39 +14,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         // User::factory(10)->create();
+        // User::factory(10)->create();
         $this->call(RoleSeeder::class);
         $this->call(EstadoLaraSeeder::class);
-        $this->call(ComunasSectoresTestDataSeeder::class);
-        $this->call(TranscripcionesTestDataSeeder::class);
 
-       Ajuste::create([
-        "nombre"=> "INNutricion",
-        "descripcion" => "Instituto Nacional de Nutrición",
-        "sucursal" => "Barquisimeto Lara",
-        "direccion" => "Calle 22 entre carrera 28 y 29, Barquisimeto, Estado Lara",
-        "telefonos" => "0251-2312345",
-        "email" => "inn.gob.ve@gmail.com",
-        "pagina_web" => "www.inn.gob.ve",
-       ]);
+        Ajuste::create([
+            'nombre' => 'INNutricion',
+            'descripcion' => 'Instituto Nacional de Nutrición',
+            'sucursal' => 'Barquisimeto Lara',
+            'direccion' => 'Calle 22 entre carrera 28 y 29, Barquisimeto, Estado Lara',
+            'telefonos' => '0251-2312345',
+            'email' => 'inn.gob.ve@gmail.com',
+            'pagina_web' => 'www.inn.gob.ve',
+        ]);
 
-       User::create([
-        'name'=>'Alejandro Alvarez',
-        'email' =>'joseale260403@gmail.com',
-        'password' => bcrypt('12345678'),
-        'telefono' => '3121234567',
-        'is_active' => true,
-       ])->assignRole('ADMINISTRADOR');
+        User::create([
+            'name' => 'Alejandro Alvarez',
+            'email' => 'joseale260403@gmail.com',
+            'password' => bcrypt('12345678'),
+            'telefono' => '3121234567',
+            'is_active' => true,
+        ])->assignRole('ADMINISTRADOR');
 
-       User::create([
-        'name'=>'Nutricionista',
-        'email' =>'nutricionista@gmail.com',
-        'password' => bcrypt('12345678'),
-        'telefono' => '3121234567',
-        'is_active' => true,
-       ])->assignRole('SECRETARIA');
+        User::create([
+            'name' => 'Nutricionista',
+            'email' => 'nutricionista@gmail.com',
+            'password' => bcrypt('12345678'),
+            'telefono' => '3121234567',
+            'is_active' => true,
+        ])->assignRole('SECRETARIA');
 
-        
         $this->command->info('Usuarios de prueba creados y rol Administrador asignado.');
     }
 }
